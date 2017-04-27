a390319ad06c3ca7bfd65a56eaf0e3ffa202b02b
friedpinch <friedpinch@gmail.com>
2017-04-27 18:28:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var przyciski = document.getElementsByClassName('button');
+for ( var k=0 ; k < przyciski.length ; k++) {
+	console.log(document.getElementsByClassName("button")[k].innerHTML);
+}
+
+
+
+
